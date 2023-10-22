a5ac8674e0681e0b5ad585b396c472e748da85fd
Jacek Zimoląg <jacek.zimolag1@gmail.com>
2023-10-22 16:17:58 +0200

    Stworzenie pustej aplikacji

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// task Day 4
+// for the gender variable k - female, m - male
+string name = "Kasia";
+char gender = 'k';
+var age = 17;
+
+if (age < 30 && gender == 'k')
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age == 33 && name == "Ewa" && gender == 'k')
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && gender == 'm')
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine(name + " lat " + age);
+}
